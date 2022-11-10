1f7aa6c7601b0315f19fd416d752431019cce461
Abhilasha-93 <aabhilasha.verma@gmail.com>
2022-11-11 00:19:21 +0530

    initial1

--- src/controller/studentController.js
+++ src/controller/studentController.js
@@ -61,15 +61,75 @@ exports.createStudent = async function (req, res) {
     }
 
 }
+/////////////////////
+// const getBook = async function (req, res) {
+//     try {
+//         const data = req.query;
 
+//         // if (!isValidRequestBody(data)) return res.status(400).send({
+//         //     status: false, message: "Invalid request parameters. Please provide book details"
+//         // })
+
+//         let obj = { isDeleted: false }
+
+//         let { userId, category, subcategory } = data;
+
+//         if (userId) {
+//             if (!isValidObjectId(userId)) {
+//                 return res.status(400).send({ status: false, msg: "userId is not valid author id please check it" })
+//             }
+//         }
+//         if (isValid(userId)) {
+//             let user = await bookModel.find({ userId: userId });
+//             if (user.length == 0) {
+//                 res.status(400).send({ status: false, msg: "no data found with this user id " })
+//                 return;
+//             }
+//             obj.userId = userId
+//         }
+
+//         if (isValid(category)) {
+//             let cat = await bookModel.find({ category: category });
+//             if (cat.length == 0) {
+//                 res.status(400).send({ status: false, msg: "category is not matching with any blog category" })
+//                 return;
+//             }
+//             obj.category = category
+//         }
+
+//         if (isValid(subcategory)) {
+//             let subcat = await bookModel.find({ subcategory: subcategory });
+//             if (subcat.length == 0) {
+//                 res.status(400).send({ status: false, msg: "subcategory is not matching with any one of blog subcategory" })
+//                 return;
+//             }
+//             obj.subcategory = subcategory
+//         }
+
+//         let findBook = await bookModel.find(obj).select({ title: 1, category: 1, excerpt: 1, userId: 1, reviews: 1, releasedAt: 1 }).sort({ title: 1 })
+//         if (findBook.length == 0) {
+//             return res.status(404).send({ status: false, message: "No such book found" })
+//         }
+//         res.status(200).send({ status: true, message: 'Books list', data: findBook })
+
+//     }
+//     catch (err) { return res.status(500).send({ message: "Error", error: err.message }) }
+
+
+
+//////////////////////
 //view
 exports.getStudent = async function (req, res) {
     let data = req.query
     //validation
+    let obj = { isDeleted: false }
+
+    let {name, subject} = data;
+  
     if(!data) 
     return res.status(400).send({ message: "enter name and subject"})
 
-    let { name, subject } = data
+   if(name){
     // validation
      //check name is present 
     
@@ -82,21 +142,24 @@ exports.getStudent = async function (req, res) {
      if (!/^([a-zA-Z. , ]){1,100}$/.test(name)) {
          return res.status(400).send({ status: false, message: `name contain only alphabets` })
      }
-     
+     obj.name=name
+   } 
      //check subject is present 
-   
+   if(subject){
      if(!subject ||typeof subject !=='string' || subject.trim().length==0 )
      {
          return res.status(400).send({ status: false, msg: "subject is required and is of string type" })
      }
     
-     
+
      //validate subject
      if (!/^([a-zA-Z. , ]){1,100}$/.test(name)) {
          return res.status(400).send({ status: false, message: `subject contain only alphabets` })
-     }
-    let studentData = await studentModel.find({ data });
-    if (!studentData) {
+     }obj.subject=subject
+    }
+
+    let studentData = await studentModel.find(obj);
+    if (studentData.length==0) {
         return res.status(404).send({ message: "student data not found" })
     }
 
@@ -108,15 +171,34 @@ exports.getStudent = async function (req, res) {
 
 exports.updateStudentData = async function (req, res) {
     
+  try  {
     let data = req.query
+ 
+   if( Object.keys(data).length == 0)
+    return res.status(400).send({ message: "enter data to edit"})
+    let { _id,name, subject, marks } = data;
+    // let filter={name,subject}
+    // let update={marks}
+    let obj = await studentModel.findOne({ _id: _id })
+  // let obj={isDeleted:false}
+   if(name){
+    obj.name=name
+   }
+   if(subject){
+    obj.subject=subject
+   }
+   if(marks){
+    obj.marks=marks
+   }
+   
+   // let updatedStudent = await studentModel.findOneAndUpdate({_id:_id},{obj}, { new: true }).save
+    await obj.save()
 
-    let { name, subject, marks } = data;
-    let filter={name,subject}
-    let update={marks}
-
-    let updatedStudent = await studentModel.findOneAndUpdate(filter,update, { new: true })
-
-    return res.status(200).send({ message: "updated student data", data: updatedStudent })
+    return res.status(200).send({ message: "updated student data", data: obj })
+}
+catch(err){
+        return res.status(500).send({status:false, message:err.message})
+    }
 }
 //delete
 
@@ -126,10 +208,8 @@ exports.deleteStudentData = async function(req,res){
     let {name,subject}=data
     if(!data) return res.status(400).send({status:false, message:"enter name and subject to delete student"});
 
-    let checkstudent=await studentModel.find({name: name, subject: subject} )
-    if(!checkstudent) return res.status(404).send({status:false, message:"Data not found"})
-    if(checkstudent.isDeleted=="true")
-    return res.status(400).send({status:false, message:"Data already deleted"})
+    let checkstudent=await studentModel.find({name: name, subject: subject, isDeleted:false} )
+    if(checkstudent.length==0) return res.status(404).send({status:false, message:"Data not found"})
 
     let deleteStudent = await studentModel.findOneAndUpdate(data, {$set:{isDeleted:true}},{ new: true })
     return res.status(200).send({ message: "Delete student data", data: deleteStudent })
